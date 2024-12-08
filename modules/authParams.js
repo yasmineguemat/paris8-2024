3edef7c502d14f1578e35429487f99735ab024e1
yasmineguemat <123726075+yasmineguemat@users.noreply.github.com>
2024-12-08 20:42:38 +0100

    ajout de authParams.js

---
+++ modules/authParams.js
@@ -0,0 +1,16 @@
+export const pa = {
+    'navbar': d3.select('#navbarMain'),
+    mail: 'thyp_24-25@api.com',
+    apiOmk: 'http://localhost/omk_thyp_24-25/api/',
+    ident: 'NAy88IyMjCcgdnoxMfVMNyj3Jhzy2WWY',
+    key: 'mxFu6luQ0JTeVxgrfVkUZRHiFzG2txtL',
+    keyGitHub: '' 
+};
+
+// paSamuel
+export const paSamuel = {
+    mail: 'samuel.szoniecky@univ-paris8.fr', 
+    apiOmk: 'http://localhost/omk_thyp_24-25/api/', 
+    ident: 'MFRiQ2ZcnRFgXT4wBnTKfDv1ou1fZoZz',
+    key: 'MhaeO914Ags46o1ZB3tKrKLRx4u1n7ps' 
+};
